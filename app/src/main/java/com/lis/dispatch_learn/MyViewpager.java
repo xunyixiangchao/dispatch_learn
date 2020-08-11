e702c95aa458ba1889cf5e51b9ad17b0051c2c6b
lis <xunyixiangchao@qq.com>
2020-08-11 23:34:49 +0800

    事件分发与解决冲突

--- app/src/main/java/com/lis/dispatch_learn/MyViewpager.java
+++ app/src/main/java/com/lis/dispatch_learn/MyViewpager.java
@@ -1,6 +1,7 @@
 package com.lis.dispatch_learn;
 
 import android.content.Context;
+import android.graphics.Matrix;
 import android.view.MotionEvent;
 
 import androidx.annotation.NonNull;
@@ -11,8 +12,37 @@ public class MyViewpager extends ViewPager {
         super(context);
     }
 
+    int mLastX;
+    int mLastY;
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        int x = (int) ev.getX();
+        int y = (int) ev.getY();
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                getParent().requestDisallowInterceptTouchEvent(true);//请求不允许中断
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                int deltaX = x - mLastX;
+                int deltaY = y - mLastY;
+                if (Math.abs(deltaX) > Math.abs(deltaY)) { //左右滑
+                    getParent().requestDisallowInterceptTouchEvent(false);//允许中断
+                }
+                break;
+        }
+        mLastX = x;
+        mLastY = y;
+        return super.dispatchTouchEvent(ev);
+    }
+
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-        return super.onInterceptTouchEvent(ev);
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            super.onInterceptTouchEvent(ev);
+            return false;
+        }
+        return true;
     }
 }
